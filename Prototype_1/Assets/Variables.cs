3569baa6cc6d52f3bc56a46d7db130d645fe18a5
Samuel Peterson <samuel.ct.peterson@gmail.com>
2022-02-15 18:49:32 -0700

    C# Variables Practice in Canvas

---
+++ Prototype_1/Assets/Variables.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name = "John";
+        Console.WriteLine(name);
+
+        int myNum = 15;
+        myNum = 20;
+        Console.WriteLine(myNum);
+
+        int newNumAgain = 5;
+        double myDoubleNum = 5.99D;
+        char myLetter = 'D';
+        bool myBool = true;
+        string myText = "Hello";
+
+        int myNewNum = 100000;
+        Console.WriteLine(myNewNum);
+
+        long newNewNum = 15000000000L;
+        Console.WriteLine(newNewNum);
+
+        float ninja = 5.75F;
+        Console.WriteLine(ninja);
+
+        double spaceship = 19.99D;
+        Console.WriteLine(spaceship);
+
+        float f1 = 35e3F;
+        double d1 = 12E4D;
+        Console.WriteLine(f1);
+        Console.WriteLine(d1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
