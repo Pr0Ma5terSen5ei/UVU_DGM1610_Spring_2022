b4a580634556a2a481852220a592060fc9ec78fc
Samuel Peterson <samuel.ct.peterson@gmail.com>
2022-02-23 20:11:25 -0700

    Switches
    Console.WriteLine code is commented out due to it causing issues in Unity

---
+++ Prototype_1/Assets/Switches.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*int day = 4;
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+        }
+        int otherDay = 4;
+        switch (otherDay)
+        {
+            case 6:
+                Console.WriteLine("Today is Saturday.");
+                break;
+            case 7:
+                Console.WriteLine("Today is Sunday.");
+                break;
+            default:
+                Console.WriteLine("Looking forward to the Weekend");
+                break;
+        }*/
+    }
+}
