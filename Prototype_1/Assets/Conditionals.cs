8aa68d159a9210071406b6d620b00c6ead7e1b3b
Samuel Peterson <samuel.ct.peterson@gmail.com>
2022-02-23 20:10:56 -0700

    Conditionals
    Unity is having issues with the Console.WriteLine command so I commented it out to prevent program issues

---
+++ Prototype_1/Assets/Conditionals.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* if (20 > 18)
+        {
+            Console.WriteLine("20 is greater than 18");
+        }
+        int x = 20;
+        int y = 18;
+        if (x > y)
+        {
+            Console.WriteLine("x is greater than y");
+        }
+        int time = 20;
+        if (time < 18)
+        {
+            Console.WriteLine("Good day.");
+        }
+        else
+        {
+            Console.WriteLine("Good evening.");
+        }
+        int newTime = 22;
+        if (newTime < 10)
+        {
+            Console.WriteLine("Good morning.");
+        }
+        else if(newTime < 20)
+        {
+            Console.WriteLine("Good day.");
+        }
+        else
+        {
+            Console.WriteLine("Good evening.");
+        }
+        int altTime = 20;
+        string result = (time < 18) ? "Good day." : "Good evening.";
+        Console.WriteLine(result);*/
+    }
+}
