8dcfd3996b272a0eb5a923e1840eede87e7e57a2
Samuel Peterson <samuel.ct.peterson@gmail.com>
2022-03-14 23:28:13 -0600

    Assignment 1

---
+++ Assignment 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Private Variables
+    private float speed = 10.0f;
+    private float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Moves car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
