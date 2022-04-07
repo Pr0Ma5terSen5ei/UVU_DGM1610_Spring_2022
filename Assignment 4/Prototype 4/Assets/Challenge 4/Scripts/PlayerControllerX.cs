afb6284457bce96ef8ac590ab7eb9ad316410102
Samuel Peterson <samuel.ct.peterson@gmail.com>
2022-04-06 20:50:11 -0600

    Challenge 4 - Soccer Scripting

---
+++ Assignment 4/Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 500;
+    private GameObject focalPoint;
+    public bool boostAvailable = true;
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+    public float boostPower = 5f;
+    public float boostCooldown = 2f;
+    public ParticleSystem smokeParticle;
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Boost();
+        }
+    }
+    // Checks if player can boost again
+    private void Boost()
+    {
+        if(boostAvailable)
+        {
+            playerRb.AddForce(focalPoint.transform.forward * boostPower, ForceMode.Impulse);
+            smokeParticle.Play();
+            boostAvailable = false;
+            StartCoroutine(Cooldown(boostCooldown, (b) => {boostAvailable = b;}));
+        }
+    }
+    // Boost cooldown
+    IEnumerator Cooldown(float cooldownTimer, System.Action<bool> thingOnCooldown)
+    {
+        yield return new WaitForSeconds(cooldownTimer);
+        thingOnCooldown(true);
+    }
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; 
+           
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+    }
+
+
+
+}
